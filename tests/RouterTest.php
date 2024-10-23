14d62daf16df97d2c4be6248546f91ce5c456a23
Agility800 <409-4@coopteh.local>
2024-10-23 13:13:46 +0700

    2 test

--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -11,4 +11,18 @@ class RouterTest extends TestCase {
         $pos= mb_strpos($html, "Создать заказ");
         $this->assertNotEquals(false, $pos);
     }
+    
+    public function test_router1() {
+        $router = new Router();
+        $html = $router->route( "http://localhost" );
+        $pos= mb_strpos($html, "Приглашаем в наш онлайн-магазин");
+        $this->assertNotEquals(false, $pos);
+    }
+    
+    public function test_router2() {
+        $router = new Router();
+        $html = $router->route( "http://localhost/product" );
+        $pos= mb_strpos($html, "Добавить в корзину");
+        $this->assertNotEquals(false, $pos);
+    }
 }
